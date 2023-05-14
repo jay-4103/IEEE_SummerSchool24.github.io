df9378c875ede35e9a71874b19980aca16fef30c
adityasinghania3802 <adityasinghania3802@gmail.com>
2023-05-14 16:47:15 +0530

    Added  Countdown

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -74,7 +74,7 @@
 
 	(function init() {
 	  function getTimeRemaining(endtime) {
-	    var t = Date.parse(endtime) - Date.parse(new Date());
+	    var t = Date.parse("2023-06-03") - Date.parse(new Date());
 	    var seconds = Math.floor((t / 1000) % 60);
 	    var minutes = Math.floor((t / 1000 / 60) % 60);
 	    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
