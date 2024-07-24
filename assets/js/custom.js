2e6d6a2d79e7f6e181494f49bf7cc0cd4dafe6e6
jay-4103 <108897556+jay-4103@users.noreply.github.com>
2024-07-24 14:43:41 +0530

    Time edited

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,106 +1,112 @@
 (function($) {
 
-	$(document).ready(function() {
-	  $('body').addClass('js');
-	  var $menu = $('#menu'),
-	    $menulink = $('.menu-link');
-	  
-	$menulink.click(function() {
-	  $menulink.toggleClass('active');
-	  $menu.toggleClass('active');
-	  return false;
-	});});
+    $(document).ready(function() {
+        $('body').addClass('js');
+        var $menu = $('#menu'),
+            $menulink = $('.menu-link');
+        
+        $menulink.click(function() {
+            $menulink.toggleClass('active');
+            $menu.toggleClass('active');
+            return false;
+        });
+    });
 
+    videoPopup();
 
-	videoPopup();
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:30,
+        nav:true,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            550:{
+                items:2
+            },
+            750:{
+                items:3
+            },
+            1000:{
+                items:4
+            },
+            1200:{
+                items:5
+            }
+        }
+    });
 
+    $(".Modern-Slider").slick({
+        autoplay:true,
+        autoplaySpeed:10000,
+        speed:600,
+        slidesToShow:1,
+        slidesToScroll:1,
+        pauseOnHover:false,
+        dots:true,
+        pauseOnDotsHover:true,
+        cssEase:'fade',
+        draggable:false,
+        prevArrow:'<button class="PrevArrow"></button>',
+        nextArrow:'<button class="NextArrow"></button>', 
+    });
 
-	$('.owl-carousel').owlCarousel({
-	    loop:true,
-	    margin:30,
-	    nav:true,
-	    autoplay:true,
-		autoplayTimeout:5000,
-		autoplayHoverPause:true,
-	    responsive:{
-	        0:{
-	            items:1
-	        },
-	        550:{
-	            items:2
-	        },
-	        750:{
-	            items:3
-	        },
-	        1000:{
-	            items:4
-	        },
-	        1200:{
-	            items:5
-	        }
-	    }
-	})
+    $("div.features-post").hover(
+        function() {
+            $(this).find("div.content-hide").slideToggle("medium");
+        },
+        function() {
+            $(this).find("div.content-hide").slideToggle("medium");
+        }
+    );
 
+    $( "#tabs" ).tabs();
 
-	$(".Modern-Slider").slick({
-	    autoplay:true,
-	    autoplaySpeed:10000,
-	    speed:600,
-	    slidesToShow:1,
-	    slidesToScroll:1,
-	    pauseOnHover:false,
-	    dots:true,
-	    pauseOnDotsHover:true,
-	    cssEase:'fade',
-	   // fade:true,
-	    draggable:false,
-	    prevArrow:'<button class="PrevArrow"></button>',
-	    nextArrow:'<button class="NextArrow"></button>', 
-	});
+    (function init() {
+        function getTimeRemaining(endtime) {
+            var now = new Date();
+            var t = Date.parse(endtime) - now.getTime();
+            console.log("Current time: " + now.toISOString());
+            console.log("End time: " + endtime);
+            console.log("Time remaining in milliseconds: " + t);
+            
+            var seconds = Math.floor((t / 1000) % 60);
+            var minutes = Math.floor((t / 1000 / 60) % 60);
+            var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+            var days = Math.floor(t / (1000 * 60 * 60 * 24));
+            
+            return {
+                'total': t,
+                'days': days,
+                'hours': hours,
+                'minutes': minutes,
+                'seconds': seconds
+            };
+        }
 
+        function initializeClock(endtime) {
+            var timeinterval = setInterval(function(){
+                var t = getTimeRemaining(endtime);
+                document.querySelector(".days > .value").innerText = t.days;
+                document.querySelector(".hours > .value").innerText = t.hours;
+                document.querySelector(".minutes > .value").innerText = t.minutes;
+                document.querySelector(".seconds > .value").innerText = t.seconds;
+                if (t.total <= 0) {
+                    clearInterval(timeinterval);
+                    document.querySelector(".days > .value").innerText = 0;
+                    document.querySelector(".hours > .value").innerText = 0;
+                    document.querySelector(".minutes > .value").innerText = 0;
+                    document.querySelector(".seconds > .value").innerText = 0;
+                }
+            }, 1000);
+        }
 
-	$("div.features-post").hover(
-	    function() {
-	        $(this).find("div.content-hide").slideToggle("medium");
-	    },
-	    function() {
-	        $(this).find("div.content-hide").slideToggle("medium");
-	    }
-	 );
-
-
-	$( "#tabs" ).tabs();
-
-
-	(function init() {
-	  function getTimeRemaining(endtime) {
-	    var t = Date.parse("2023-06-03") - Date.parse(new Date());
-	    var seconds = Math.floor((t / 1000) % 60);
-	    var minutes = Math.floor((t / 1000 / 60) % 60);
-	    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-	    var days = Math.floor(t / (1000 * 60 * 60 * 24));
-	    return {
-	      'total': t,
-	      'days': days,
-	      'hours': hours,
-	      'minutes': minutes,
-	      'seconds': seconds
-	    };
-	  }
-	  
-	  function initializeClock(endtime){
-	  var timeinterval = setInterval(function(){
-	    var t = getTimeRemaining(endtime);
-	    document.querySelector(".days > .value").innerText=t.days;
-	    document.querySelector(".hours > .value").innerText=t.hours;
-	    document.querySelector(".minutes > .value").innerText=t.minutes;
-	    document.querySelector(".seconds > .value").innerText=t.seconds;
-	    if(t.total<=0){
-	      clearInterval(timeinterval);
-	    }
-	  },1000);
-	}
-	initializeClock(((new Date()).getFullYear()+1) + "/1/1")
-	})()
+        // Use ISO 8601 format for UTC: YYYY-MM-DDTHH:MM:SSZ
+        initializeClock("2024-08-04T00:00:00Z");
+    })();
 
 })(jQuery);
